26f8e42d16d56d959b54526f5c7aa3f8502664af
Davi Moreno da Silva <davisilva.2525moreno@gmail.com>
2025-06-14 03:23:16 -0300

    Novos programas

---
+++ desafio03.py
@@ -0,0 +1,5 @@
+num1 = int(input("Digite um número qualquer: "))
+num2 = int(input("Digite um outro número: "))
+sum = num1 + num2
+
+print("{} + {} = {}".format(num1,num2,sum))
---
+++ desafio04.py
@@ -0,0 +1,3 @@
+nome = str(input("Qual é o seu nome? "))
+
+print("Seja Bem-vindo, meu caro {}".format(nome))
