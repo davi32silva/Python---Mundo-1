0ba5b7ba1cee2cdacf5b895b52aed13702ea7e6c
Davi Moreno da Silva <davisilva.2525moreno@gmail.com>
2025-06-14 04:14:42 -0300

    Desafio 07

---
+++ desafio07.py
@@ -0,0 +1,6 @@
+num1 = float(input("Primeira nota do aluno: "))
+num2 = float(input("Segunda nota do aluno: "))
+num3 = float(input("Terceira nota do aluno: "))
+media = (num1 + num2 + num3) / 3
+
+print("A média das notas é {}".format(media))
